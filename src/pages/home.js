bb8d104d530e533f0b67d85ca9940b80a618137a
pepper-sister <yuyu7123@naver.com>
2025-11-12 06:32:51 +0900

    refactor: 함수 하나로 통합

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,22 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import '../styles/home.css';
-import mainImage from '../assets/main.jpg';
-import univIcon from '../assets/univ.png';
-import potalIcon from '../assets/potal.png';
-import campusIcon from '../assets/campus.png';
-import bookIcon from '../assets/book.png';
+import { useState, useEffect } from "react";
+import "../styles/home.css";
 
-const Home = () => {
-  const [time, setTime] = useState('');
+import mainImage from "../assets/main.jpg";
+import univIcon from "../assets/univ.png";
+import potalIcon from "../assets/potal.png";
+import campusIcon from "../assets/campus.png";
+import bookIcon from "../assets/book.png";
+
+export default function Home() {
+  const [time, setTime] = useState("");
 
   useEffect(() => {
     const updateClock = () => {
       let currentTime = new Date();
       currentTime.setSeconds(currentTime.getSeconds() + 1.1);
 
-      const hours = String(currentTime.getHours()).padStart(2, '0');
-      const minutes = String(currentTime.getMinutes()).padStart(2, '0');
-      const seconds = String(currentTime.getSeconds()).padStart(2, '0');
+      const hours = String(currentTime.getHours()).padStart(2, "0");
+      const minutes = String(currentTime.getMinutes()).padStart(2, "0");
+      const seconds = String(currentTime.getSeconds()).padStart(2, "0");
 
       setTime(`${hours}:${minutes}:${seconds}`);
     };
@@ -27,56 +28,40 @@ const Home = () => {
     return () => clearInterval(intervalId);
   }, []);
 
-  const goToSejongHomepage = () => {
-    window.open('http://www.sejong.ac.kr/', '_blank');
-  };
-
-  const goToSejongPortal = () => {
-    window.open('https://portal.sejong.ac.kr/jsp/login/loginSSL.jsp', '_blank');
-  };
-
-  const goToJiphyunCampus = () => {
-    window.open('https://ecampus.sejong.ac.kr/', '_blank');
-  };
-
-  const goToLibrary = () => {
-    window.open('https://library.sejong.ac.kr/index.ax', '_blank');
+  const goPage = (link) => {
+    window.open(`${link}`, "_black");
   };
 
   return (
-    <div>
-      <main>
-        <div class="main-section">
-          <div class="mainimg-section">
-            <img src={mainImage} alt="" />
-            <div class="clock-container">
-              <div class="clock-section">학교 서버시간</div>
-              <h2 id="clock" class="clock">{time}</h2>
-            </div>
-          </div>
+    <div className="home">
+      <img className="home__img" src={mainImage} alt="" />
 
-          <div class="mainside-bar">
-            <button class="mainsidebar-button" onClick={goToSejongHomepage}>
-              <img src={univIcon} alt="" class="icon" />
-              세종대 홈페이지
-            </button>
-            <button class="mainsidebar-button" onClick={goToSejongPortal}>
-              <img src={potalIcon} alt="" class="icon" />
-              세종대 포털
-            </button>
-            <button class="mainsidebar-button" onClick={goToJiphyunCampus}>
-              <img src={campusIcon} alt="" class="icon" />
-              집현캠퍼스
-            </button>
-            <button class="mainsidebar-button" onClick={goToLibrary}>
-              <img src={bookIcon} alt="" class="icon" />
-              학술정보원
-            </button>
-          </div>
-        </div>
-      </main>
+      <div className="clock__section">
+        <div className="clock__text">학교 서버시간</div>
+        <h2 className="clock">{time}</h2>
+      </div>
+
+      <div className="home__feature">
+        <button className="home__feature__button" onClick={() => goPage("http://www.sejong.ac.kr/")}>
+          <img src={univIcon} alt="" className="home__feature__icon" />
+          <p className="home__feature__text">세종대 홈페이지</p>
+        </button>
+        <button
+          className="home__feature__button"
+          onClick={() => goPage("https://portal.sejong.ac.kr/jsp/login/loginSSL.jsp")}
+        >
+          <img src={potalIcon} alt="" className="home__feature__icon" />
+          <p className="home__feature__text">세종대 포털</p>
+        </button>
+        <button className="home__feature__button" onClick={() => goPage("https://ecampus.sejong.ac.kr/")}>
+          <img src={campusIcon} alt="" className="home__feature__icon" />
+          <p className="home__feature__text">집현캠퍼스</p>
+        </button>
+        <button className="home__feature__button" onClick={() => goPage("https://library.sejong.ac.kr/index.ax")}>
+          <img src={bookIcon} alt="" className="home__feature__icon" />
+          <p className="home__feature__text">학술정보원</p>
+        </button>
+      </div>
     </div>
   );
-};
-
-export default Home;
+}
