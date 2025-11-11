9201c2af51c9277728b806d003cceddfa6fd82eb
pepper-sister <yuyu7123@naver.com>
2025-11-12 06:33:32 +0900

    refactor: 지도 API 불러오기 수정

--- src/pages/facilities.js
+++ src/pages/facilities.js
@@ -1,17 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import '../styles/facilities.css';
+import { useEffect, useState } from "react";
+import "../styles/facilities.css";
 
-const Facilities = () => {
+export default function Facilities() {
   const [map, setMap] = useState(null);
   const [markers, setMarkers] = useState([]);
 
   useEffect(() => {
     const loadKakaoMapScript = () => {
-      const script = document.createElement('script');
-      script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_MAP_KEY}&autoload=false`;
-      script.onload = () => {
-        window.kakao.maps.load(initMap);
-      };
+      if (window.kakao && window.kakao.maps) {
+        initMap();
+        return;
+      }
+
+      const appKey = process.env.REACT_APP_KAKAO_MAP_KEY;
+      const script = document.createElement("script");
+      script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${appKey}&autoload=false&libraries=services`;
+      script.onload = () => window.kakao.maps.load(initMap);
       document.head.appendChild(script);
     };
 
@@ -19,7 +23,7 @@ const Facilities = () => {
   }, []);
 
   const initMap = () => {
-    const mapContainer = document.getElementById('map');
+    const mapContainer = document.getElementById("map");
     const mapOption = {
       center: new window.kakao.maps.LatLng(37.5507885, 127.0739328),
       level: 3,
@@ -32,23 +36,23 @@ const Facilities = () => {
   const showMarker = (type) => {
     removeAllMarkers();
 
-    if (type === 'printer') {
-      addMarker(new window.kakao.maps.LatLng(37.551538, 127.074238), '<div>학술정보원 2층/5층/6층/9층</div>');
-      addMarker(new window.kakao.maps.LatLng(37.551089, 127.0757574), '<div>대양AI센터 B1층</div>');
-      addMarker(new window.kakao.maps.LatLng(37.5495386, 127.0737384), '<div>군자관 1층</div>');
-      addMarker(new window.kakao.maps.LatLng(37.5522605, 127.0739606), '<div>충무관 1층</div>');
-      addMarker(new window.kakao.maps.LatLng(37.5494831, 127.0750716), '<div>학생회관 1층</div>');
-      addMarker(new window.kakao.maps.LatLng(37.550106, 127.073171), '<div>광개토관 1층</div>');
-    } else if (type === 'atm') {
-      addMarker(new window.kakao.maps.LatLng(37.5495386, 127.0737384), '<div>군자관 1층</div>');
-      addMarker(new window.kakao.maps.LatLng(37.550106, 127.073171), '<div>광개토관 1층</div>');
-      addMarker(new window.kakao.maps.LatLng(37.5522605, 127.0739606), '<div>충무관 1층</div>');
-    } else if (type === 'convenience-store') {
-      addMarker(new window.kakao.maps.LatLng(37.551089, 127.0757574), '<div>대양AI센터 1층</div>');
-      addMarker(new window.kakao.maps.LatLng(37.5494831, 127.0750716), '<div>학생회관 1층</div>');
-      addMarker(new window.kakao.maps.LatLng(37.550106, 127.073171), '<div>광개토관 B1층</div>');
-      addMarker(new window.kakao.maps.LatLng(37.5518161, 127.0740439), '<div>율곡관 B1층</div>');
-      addMarker(new window.kakao.maps.LatLng(37.5508718, 127.073294), '<div>진관홀 B1층</div>');
+    if (type === "printer") {
+      addMarker(new window.kakao.maps.LatLng(37.551538, 127.074238), "<div>학술정보원 2층/5층/6층/9층</div>");
+      addMarker(new window.kakao.maps.LatLng(37.551089, 127.0757574), "<div>대양AI센터 B1층</div>");
+      addMarker(new window.kakao.maps.LatLng(37.5495386, 127.0737384), "<div>군자관 1층</div>");
+      addMarker(new window.kakao.maps.LatLng(37.5522605, 127.0739606), "<div>충무관 1층</div>");
+      addMarker(new window.kakao.maps.LatLng(37.5494831, 127.0750716), "<div>학생회관 1층</div>");
+      addMarker(new window.kakao.maps.LatLng(37.550106, 127.073171), "<div>광개토관 1층</div>");
+    } else if (type === "atm") {
+      addMarker(new window.kakao.maps.LatLng(37.5495386, 127.0737384), "<div>군자관 1층</div>");
+      addMarker(new window.kakao.maps.LatLng(37.550106, 127.073171), "<div>광개토관 1층</div>");
+      addMarker(new window.kakao.maps.LatLng(37.5522605, 127.0739606), "<div>충무관 1층</div>");
+    } else if (type === "convenience-store") {
+      addMarker(new window.kakao.maps.LatLng(37.551089, 127.0757574), "<div>대양AI센터 1층</div>");
+      addMarker(new window.kakao.maps.LatLng(37.5494831, 127.0750716), "<div>학생회관 1층</div>");
+      addMarker(new window.kakao.maps.LatLng(37.550106, 127.073171), "<div>광개토관 B1층</div>");
+      addMarker(new window.kakao.maps.LatLng(37.5518161, 127.0740439), "<div>율곡관 B1층</div>");
+      addMarker(new window.kakao.maps.LatLng(37.5508718, 127.073294), "<div>진관홀 B1층</div>");
     }
   };
 
@@ -62,11 +66,11 @@ const Facilities = () => {
       content: title,
     });
 
-    window.kakao.maps.event.addListener(marker, 'mouseover', () => {
+    window.kakao.maps.event.addListener(marker, "mouseover", () => {
       infowindow.open(map, marker);
     });
 
-    window.kakao.maps.event.addListener(marker, 'mouseout', () => {
+    window.kakao.maps.event.addListener(marker, "mouseout", () => {
       infowindow.close();
     });
 
@@ -81,20 +85,20 @@ const Facilities = () => {
   };
 
   return (
-    <div>
-      <main>
-        <div class="fac-section">
-          <div id="map"></div>
+    <div className="facilities">
+      <div id="map" className="facilities__map"></div>
 
-          <div class="button-section">
-            <button class="fac-button" onClick={() => showMarker('printer')}>프린터</button>
-            <button class="fac-button" onClick={() => showMarker('atm')}>ATM</button>
-            <button class="fac-button" onClick={() => showMarker('convenience-store')}>편의점</button>
-          </div>
-        </div>
-      </main>
+      <div className="facilities__buttons">
+        <button className="facilities__button" onClick={() => showMarker("printer")}>
+          프린터
+        </button>
+        <button className="facilities__button" onClick={() => showMarker("atm")}>
+          ATM
+        </button>
+        <button className="facilities__button" onClick={() => showMarker("convenience-store")}>
+          편의점
+        </button>
+      </div>
     </div>
   );
-};
-
-export default Facilities;
+}
